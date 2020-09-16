745058ada532d3c553acfc8dc906f8b705f45c37
Mike Neder <mneder@spamarrest.com>
2020-09-16 21:21:55 +0700

    DEV: [POM-WEB] Test mapping domain #17

--- dns_mapping.sh
+++ dns_mapping.sh
@@ -1,2 +1,17 @@
 #!/bin/sh
-curl -X PUT -H 'X-NSONE-Key: $XNSONEKEY' -d '{"zone":"proudofmom.com", "domain":"stage$PR_NUMBER.proudofmom.com", "type":"CNAME", "answers":[{"answer":["ghs.googlehosted.com."]}]}' https://api.nsone.net/v1/zones/proudofmom.com/stage$PR_NUMBER.proudofmom.com/CNAME
+curl -X PUT 'https://api.nsone.net/v1/zones/proudofmom.com/stage${PR_NUMBER}.proudofmom.com/CNAME' \
+-H 'X-NSONE-Key: ${XNSONEKEY}' \
+-H 'Content-Type: application/json' \
+-H 'Cookie: __cfduid=d3fba17ce77656e5203590e843f78499c1600179264' \
+-d '{
+    "zone": "proudofmom.com",
+    "domain": "stage${PR_NUMBER}.proudofmom.com",
+    "type": "CNAME",
+    "answers": [
+        {
+            "answer": [
+                "ghs.googlehosted.com"
+            ]
+        }
+    ]
+}'
