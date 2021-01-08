5d967e97e729a5d41be95286eafb534247144e77
Mike Neder <mneder@spamarrest.com>
2021-01-08 23:54:19 +0700

    wip

--- src/components/Avatar/Avatar.tsx
+++ src/components/Avatar/Avatar.tsx
@@ -3,7 +3,7 @@ import { Avatar as Instance, AvatarProps } from 'theme-ui';
 export interface IAvatar extends AvatarProps {
   dataTestId: string;
 }
-export const Avatar = ({ dataTestId, ...props }: IAvatar) => (
+export const Avatar = ({ dataTestId, ...props }: IAvatar & any) => (
   <Instance
     style={{ border: '5px solid #ccc', width: 100, height: 100 }}
     data-testid={dataTestId}
